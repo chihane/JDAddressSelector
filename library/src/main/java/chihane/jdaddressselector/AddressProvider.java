b4cb70d54b525a3086ef76bc50e0ccc27cb2fd79
chihane <chihane@yeah.net>
2016-06-30 10:59:22 +0800

    Rename param names

--- library/src/main/java/chihane/jdaddressselector/AddressProvider.java
+++ library/src/main/java/chihane/jdaddressselector/AddressProvider.java
@@ -10,8 +10,8 @@ import chihane.jdaddressselector.model.Street;
 public interface AddressProvider {
     void provideProvinces(AddressReceiver<Province> addressReceiver);
     void provideCitiesWith(int provinceId, AddressReceiver<City> addressReceiver);
-    void provideCountiesWith(int countyId, AddressReceiver<County> addressReceiver);
-    void provideStreetsWith(int streetId, AddressReceiver<Street> addressReceiver);
+    void provideCountiesWith(int cityId, AddressReceiver<County> addressReceiver);
+    void provideStreetsWith(int countyId, AddressReceiver<Street> addressReceiver);
 
     interface AddressReceiver<T> {
         void send(List<T> data);
